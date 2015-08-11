c043c37254a13ddcb663dea6425842054bc742d4
Van Nguyen <van.nguyenthi@asnet.com.vn>
2015-08-11 19:27:21 +0700

    Apply Refills to before practices (Navigation, hero, bullet points, footer)

--- frontend/html-css/business-new/website/app/scripts/main.js
+++ frontend/html-css/business-new/website/app/scripts/main.js
@@ -1,2 +1,13 @@
-// jshint devel:true
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
