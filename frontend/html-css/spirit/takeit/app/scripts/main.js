5378b317f7c13e3c72498f871214c4d3db0d4b78
Van Nguyen <van.nguyenthi@asnet.com.vn>
2015-09-10 19:29:08 +0700

    finished style for screen-three and start style for screen-four

--- frontend/html-css/spirit/takeit/app/scripts/main.js
+++ frontend/html-css/spirit/takeit/app/scripts/main.js
@@ -5,16 +5,19 @@ Takeit = {
     menu: $("#menu"),
     nav: $("#nav"),
     overlay: $('#overlay'),
+    pics: $('#pics'),
     scr: $('.screen'),
     screenone: $('.screen-one'),
     screentwo: $('.screen-two'),
-    screenthree: $('.screen-three')
+    screenthree: $('.screen-three'),
+    screenfour: $('.screen-four')
 
   },
 
   init: function() {
     // kick things off
     s = this.settings;
+    s.screenone.addClass('active');
   },
 
   bindMenuActions: function() {
@@ -49,64 +52,65 @@ Takeit.init();
 Takeit.bindMenuActions();
 
 
-// $(document).ready (function () {
-//   // initialize swiper when document ready
-//   var mySwiper = new Swiper ('.swiper-container', {
-//     pagination: '.swiper-pagination',
-//     direction: 'vertical',
-//     slidesPerView: 1,
-//     paginationClickable: true,
-//     spaceBetween: 30,
-//     mousewheelControl: true
-//   })
-//   mySwiper.on('slideChangeStart', function () {
-//     console.log('slide change start');
-//   });
-// });
 
-$(window).bind('mousewheel', function(event) {
+$('#screen').bind('mousewheel', function(event) {
     var src1 = s.screenone.hasClass('active'),
         src2 = s.screentwo.hasClass('active'),
-        src3 = s.screenthree.hasClass('active');
+        src3 = s.screenthree.hasClass('active'),
+        src4 = s.screenfour.hasClass('active');
+
+
+  clearTimeout($.data(this, 'timer'));
+
 
     if (event.originalEvent.wheelDelta >= 0) {
-    console.log('Scroll up');
-    setTimeout(function() {
+
+
       if (src2) {
-        s.screentwo.removeClass('active');
-        s.screentwo.addClass('bottom');
-        s.screenone.addClass('active');
+        $.data(this, 'timer', setTimeout(function() {
         s.screenone.removeClass('top');
+        s.screenone.addClass('active');
+        s.screentwo.removeClass('active');
+        s.pics.removeClass('state-one');
+        s.screentwo.addClass('bottom');
+        }, 1000));
       }
-     }, 1000);
-    setTimeout(function() {
+
       if (src3) {
+        $.data(this, 'timer', setTimeout(function() {
+        s.screentwo.removeClass('top');
+        s.screentwo.addClass('active');
+        s.pics.addClass('state-one');
         s.screenthree.removeClass('active');
         s.screenthree.addClass('bottom');
-        s.screentwo.addClass('active');
-        s.screentwo.removeClass('top');
+        s.pics.removeClass('state-two');
+        }, 1000));
       }
-    }, 1000);
-  }
-  else {
-    console.log('Scroll down');
-    setTimeout(function() {
+    }
+    else {
+
       if (src1) {
+        $.data(this, 'timer', setTimeout(function() {
+        s.screentwo.removeClass('bottom');
+        s.screentwo.addClass('active');
+        s.pics.addClass('state-one');
         s.screenone.removeClass('active');
         s.screenone.addClass('top');
-        s.screentwo.addClass('active');
-        s.screentwo.removeClass('bottom');
+        }, 1000));
       }
-    }, 1000);
-    setTimeout(function() {
+
       if (src2) {
-        s.screentwo.removeClass('active');
-        s.screentwo.addClass('top');
-        s.screenthree.addClass('active');
+        $.data(this, 'timer', setTimeout(function() {
         s.screenthree.removeClass('bottom');
+        s.screenthree.addClass('active');
+        s.pics.addClass('state-two');
+        s.screentwo.removeClass('active');
+        s.pics.removeClass('state-one');
+        s.screentwo.addClass('top');
+        }, 200));
       }
-    }, 1000);
-  }
+    }
+
 });
 
 
@@ -115,3 +119,4 @@ $(document).on('click touchstart', function () {
   $('#nav').removeClass('displayed');
   $('#overlay').removeClass('displayed');
 });
+
