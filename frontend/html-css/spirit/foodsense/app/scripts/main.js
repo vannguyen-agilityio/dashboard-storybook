77e7d3fc832251d4e024fc7e6c1ca22b32840e1e
Van Nguyen <van.nguyenthi@asnet.com.vn>
2015-10-05 10:26:28 +0700

    Add gulpconfig.sh

--- frontend/html-css/spirit/foodsense/app/scripts/main.js
+++ frontend/html-css/spirit/foodsense/app/scripts/main.js
@@ -1,9 +1,10 @@
 var s,
-Takeit = {
+Foodsense = {
 
   settings: {
     $body: $('body'),
-    $slide: $('#slides')
+    $slide: $('#slides'),
+    $navSlide: $('#nav-slide')
   },
 
   init: function() {
@@ -15,11 +16,15 @@ Takeit = {
 };
 
 
-Takeit.init();
+Foodsense.init();
 var slideOne = s.$slide.find('.slide-1'),
   slideTwo = s.$slide.find('.slide-2'),
   slideThree = s.$slide.find('.slide-3'),
-  slideFour = s.$slide.find('.slide-4');
+  slideFour = s.$slide.find('.slide-4'),
+  featureOne = s.$navSlide.find('.feature-1'),
+  featureTwo = s.$navSlide.find('.feature-2'),
+  featureThree = s.$navSlide.find('.feature-3'),
+  featureFour = s.$navSlide.find('.feature-4');
 
 setInterval(function(){
   var slideActive = s.$slide.find('section.active');
@@ -28,18 +33,22 @@ setInterval(function(){
     case slideActive.is(slideOne):
       slideOne.removeClass('active');
       slideTwo.addClass('active');
+      featureTwo.addClass('active');
       break;
     case slideActive.is(slideTwo):
       slideTwo.removeClass('active');
       slideThree.addClass('active');
+      featureThree.addClass('active');
       break;
     case slideActive.is(slideThree):
       slideThree.removeClass('active');
       slideFour.addClass('active');
+      featureFour.addClass('active');
       break;
     case slideActive.is(slideFour):
       slideFour.removeClass('active');
       slideOne.addClass('active');
+      featureOne.addClass('active');
     break;
   }
 
